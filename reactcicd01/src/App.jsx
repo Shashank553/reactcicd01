f9553ea5bf36490961dbf2cb3c697ab101014ff1
Shashank553 <2300033058@kluniversity.in>
2025-08-04 09:59:52 +0530

    Your message here

--- reactcicd01/src/App.jsx
+++ reactcicd01/src/App.jsx
@@ -1,23 +1,19 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <div className="logos">
+        <img src={reactLogo} className="logo react" alt="React logo" />
+        <img src={reactLogo} className="logo react" alt="React logo" />
       </div>
       <h1>Vite + React</h1>
-      <h2>Hi this is GIT ACTION EXPERIMENT</h2>
+      <h2 className="highlight-text">Hi This is  second version GIT ACTION EXPERIMENT</h2>
+        <h2 className="highlight-text">Hi This is  second version GIT ACTION EXPERIMENT</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
